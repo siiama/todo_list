584b8c14ada9d57dfe0354051a86bc831eabaa8b
siia <nmarkova273@gmail.com>
2023-03-27 14:23:32 +0300

    add home page and user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,18 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV.fetch('SMTP_USER_NAME'),
+    password: ENV.fetch('SMTP_PASSWORD'),
+    address: ENV.fetch('SMTP_ADDRESS'),
+    domain: ENV.fetch('SMTP_DOMAIN'),
+    port: ENV.fetch('SMTP_PORT'),
+    authentication: :cram_md5
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
